092f8b43dc1e940e6bcdf455050ccc78f2c4cfe4
David Taylor <david@taylorhq.com>
2024-02-20 09:47:17 +0000

    DEV: Refactor for core changes
    Core's script helper methods expect to be run in the context of a controller, with access to the response property. This is especially important now that core is applying a CSP nonce to every `<script>`.

--- lib/multilingual/locale_loader.rb
+++ lib/multilingual/locale_loader.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 class ::Multilingual::LocaleLoader
-  include ::ApplicationHelper
+  attr_reader :controller
 
-  attr_reader :ctx
-
-  def initialize(ctx)
-    @ctx = ctx
+  def initialize(controller)
+    @controller = controller
   end
 
   def request
-    @ctx && @ctx.request ? @ctx.request : ActionDispatch::Request.new
+    @controller.request
   end
 
   def asset_path(url)
-    ActionController::Base.helpers.asset_path(url)
+    @controller.helpers.asset_path(url)
   end
 
   def current_locale
@@ -25,14 +23,14 @@ class ::Multilingual::LocaleLoader
   end
 
   def preload_i18n
-    preload_script("locales/i18n")
+    @controller.helpers.preload_script("locales/i18n")
   end
 
   def preload_custom_locale
-    preload_script_url(ExtraLocalesController.url('custom-language'))
+    @controller.helpers.preload_script_url(ExtraLocalesController.url("custom-language"))
   end
 
   def preload_tag_translations
-    preload_script_url(ExtraLocalesController.url('tags'))
+    @controller.helpers.preload_script_url(ExtraLocalesController.url("tags"))
   end
 end
